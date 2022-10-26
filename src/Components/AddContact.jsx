56cdde68660ced48709e960dba0b735091866fd4
Mr.Smoke <kumroshan120@gmail.com>
2022-10-26 22:44:10 +0530

    Add Header and add form

--- src/Components/AddContact.jsx
+++ src/Components/AddContact.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export const AddContact = () => {
-  return <div>AddContact</div>;
+  return (
+    <div className="ui main">
+      <h2>Add Contact</h2>
+      <form action="" className="ui form">
+        <div className="filed">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" placeholder="Enter name" />
+        </div>
+        <div className="filed">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Enter email" />
+        </div>
+        <button className="ui button blue">Add</button>
+      </form>
+    </div>
+  );
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div size="huge" className="ui menu">
+      <div className="ui container center" size="huge">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
 };
